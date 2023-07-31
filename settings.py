aa9507767954ab8f737b676065b3c28db735b271
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-07-31 19:38:25 +0300

    Добавлен файл с классом настроек

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Класс для хранения всех настроек игры"""
+    def __init__(self):
+        """Инициализирует настройки игры"""
+        #Параметры экрана
+        self.scree_widht = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
