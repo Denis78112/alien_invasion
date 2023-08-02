2355a2af359eec6adb6e418831ad7f3b88522ede
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-02 11:29:00 +0300

    Изменение скорости корабля на более плавное движение

--- settings.py
+++ settings.py
@@ -6,3 +6,6 @@ class Settings():
         self.screen_widht = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        #Настройки корабля
+        self.ship_speed = 1.5
+        
--- ship.py
+++ ship.py
@@ -6,22 +6,32 @@ class Ship():
         """Инициализирует корабль и задает его начальную позицию"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
         
         #Загружает изображение корабля и получает прямоугольник.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
         #Каждый новый корабль появляется у нижнего крапя экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #Сохранение вещественной координаты центра корабля
+        self.x = float(self.rect.x)
+
         #Флаг перемещения
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Обновляет позицию коробля с учетом флага"""
+        #Обновляет атрибут х а не rect.
+
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        #Обновление атрибута rect на основе self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """рисует корабль в текущей позиции"""
