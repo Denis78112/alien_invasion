dc99b5ea6fbb6e976fe17d444df4668b0eb2ec36
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-02 12:17:47 +0300

    Теперь корабль не выйдет за пределы экрана

--- ship.py
+++ ship.py
@@ -26,9 +26,10 @@ class Ship():
         """Обновляет позицию коробля с учетом флага"""
         #Обновляет атрибут х а не rect.
 
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         #Обновление атрибута rect на основе self.x.
         self.rect.x = self.x
