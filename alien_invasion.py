858d3f180ec869d4373d8399727cdbc7b58126d5
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-02 11:08:55 +0300

    Добавлено непрерывное движение вправо.

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,23 +13,46 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_widht, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        self.ship = Ship(screen)
+        self.ship = Ship(self)
 
 
     def run_game(self):
         """Запуск основоного цикла игры"""
         while True:
             #Отлслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
             #при каждом проходе цикла перерисовывается экран
             self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        """Отрабатывает нажатия клавиш и события мыши"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #Переместить корабль вправо.
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
+                
+
+    def _update_screen(self):
+         """Обновляет изображение на экране и отображает новый экран"""
+         self.screen.fill(self.settings.bg_color)
+         self.ship.blitme()
             
 
-            #Отображение последнего прорисованного экрана.
-            pygame.display.flip()
+        #Отображение последнего прорисованного экрана.
+         pygame.display.flip()
+
+            
+
 
 if __name__ == '__main__':
     #Создание экзепляра класса и запуск игры.
--- ship.py
+++ ship.py
@@ -8,10 +8,17 @@ class Ship():
         self.screen_rect = ai_game.screen.get_rect()
         
         #Загружает изображение корабля и получает прямоугольник.
-        self.image = pygame.image.load('images/crash_course12-01.png')
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         #Каждый новый корабль появляется у нижнего крапя экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+        #Флаг перемещения
+        self.moving_right = False
+
+    def update(self):
+        """Обновляет позицию коробля с учетом флага"""
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         """рисует корабль в текущей позиции"""
