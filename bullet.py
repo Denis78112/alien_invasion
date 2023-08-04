5d7b1da8f57bcacdbc7404bf7e8dcc40ecc6ebd1
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-04 11:22:37 +0300

    создание класса снаряда и настроек снаряда

---
+++ bullet.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """Класс для управления снарядами, выпущенными кораблем"""
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        #Создание снарядв в позиции (0,0) и назначение правильной позиции
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        #Позици снаряда хранится в вещественном формате.
+        self.y = float(self.rect.y)
+        
