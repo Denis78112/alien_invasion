357562b14d9698987f4be76340cad78042e3034d
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-05 19:09:50 +0300

    Создание настроек направления пришельцев

--- settings.py
+++ settings.py
@@ -18,5 +18,7 @@ class Settings():
 
         #Настройки пришельцев
         self.alien_speed = 1.0
-        
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 обозначает движение вправо; а -1 влево
+        self.fleet_direction = 1
 
