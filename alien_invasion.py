16e0be2a73c27c352d659cf74f3fdd453aad1113
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-05 12:11:22 +0300

    ограничение колличества снарядо на экране, изменения в settings и alien_invion

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,7 +34,7 @@ class AlienInvasion:
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
 
-            print(len(self.bullets))
+           
 
             self._update_screen()
 
@@ -76,8 +76,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Создание нового снаряда и включение его в группу bullets."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)       
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)       
 
     def _update_screen(self):
          """Обновляет изображение на экране и отображает новый экран"""
