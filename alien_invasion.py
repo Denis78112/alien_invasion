10d3f5828b495bc2dbee265eea99c675d06a6f5d
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-05 18:52:21 +0300

    Создание флота пришельцев

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,21 +93,30 @@ class AlienInvasion:
         #создание пришельца и вычисление колличества пришельцев в ряду
         # интервал между соседними пришельцами равен ширине пришельца.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_widht - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
-        
-        #Создание первого ряда пришельцев
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
 
-    def _create_alien(self, alien_number):
+        """Определяет колличество рядов, помещающихся на экране."""
+        ship_height = self.ship.rect.height
+        avilable_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = avilable_space_y // (2 * alien_height)
+
+              
+        #Создание флота вторжения
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
         """Создание пришельца и размещение его в ряду"""
         #Создание первого ряда пришельцев
         alien = Alien(self)
-        alien_width = alien.rect.width
+
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
       
 
