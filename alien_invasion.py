f9cebb70be3e119c09f5f92a28f0db16011943c1
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-05 19:42:19 +0300

    Снижение флота и смена направления

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,8 +81,9 @@ class AlienInvasion:
 
     def _update_aliens(self):
         """Обновляет позиции всех пришельцев."""
+        self._check_fleet_edges()
         self.aliens.update()
-        
+
 
     def _update_bullets(self):
         """Обновляет позиции снарядов и уничтожает снаряды"""
@@ -124,7 +125,22 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
-      
+
+    def _check_fleet_edges(self):
+        """"Реагирует на достижение пришельцами края экрана"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и менят направление флота."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран"""
