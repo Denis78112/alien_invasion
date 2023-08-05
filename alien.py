12414628635bb913d51d32071304b9e89b384455
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-05 12:52:54 +0300

    Создание класса пришельца и назначение его начальной позиции

---
+++ alien.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """Класс представляющий одного пришельца."""
+    def __init__(self, ai_game):
+        """Инициализирует пришельца и задает его наяальную позицию."""
+        super().__init__()
+        self.screen = ai_game.screen
+        #Загрузка изображения пришельца и назначение атрибута rect.
+        self.image = pygame.image.load('image/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        #Каджый пришелец появляется в левом верхнем углу экрана.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        #сохранение точной горизонтальной позиции пришельца.
+        self.x = float(self.rect.x)
+
+        
