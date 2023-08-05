bc44ba4fdb894732c3d00b0a00ae980bc14dcb6b
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-05 19:18:40 +0300

    проверка достижения пришельцами края экрана.

--- alien.py
+++ alien.py
@@ -20,9 +20,16 @@ class Alien(Sprite):
         #сохранение точной горизонтальной позиции пришельца.
         self.x = float(self.rect.x)
 
+    def check_edges(self):
+        """Возвращает True усли пришелец находится у края экрана."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
     def update(self):
         """Перемещение пришельца вправо."""
-        self.x += self.settings.alien_speed
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
-        
+    
+    
 
