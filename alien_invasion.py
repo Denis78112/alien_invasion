c64d1ef635a90cca8d5581021cbfe3ce6794c4d8
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-07 15:27:10 +0300

    кнопка Play активна

--- alien_invasion.py
+++ alien_invasion.py
@@ -64,6 +64,16 @@ class AlienInvasion:
                                 
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """запускает новую игру при нажатии кнопки PLAY"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
+
+
 
     def _check_keydown_events(self, event):
         """Реагирует на нажатие клавиш."""
