5e3518c7bad2379c19ac6ebc34f1b211f59278e1
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-07 14:42:30 +0300

    Добавлен новый файл button где описан класс кнопки.

--- game_stats.py
+++ game_stats.py
@@ -6,8 +6,8 @@ class GameStats():
         self.reset_stats()
 
         #игра запускается в активном состоянии.
-        self.game_active = True
-        
+        self.game_active = False
+
 
     def reset_stats(self):
         """инициализирует статистику изменяющуюся в ходе игры"""
