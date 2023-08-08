31269e48e1f53fba09df0ad8ebd6f02f928d037d
Denis78112 <98334670+Denis78112@users.noreply.github.com>
2023-08-08 09:53:26 +0300

    исправлена ошибка при вызове метода chek_high_score

--- alien_invasion.py
+++ alien_invasion.py
@@ -197,7 +197,7 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
-            self.check_high_score()
+            self.sb.check_high_score()
             
 
 
